61714fb214c8eb275205fb01d33c4a10a60d7aed
ImaadDean <123092643+ImaadDean@users.noreply.github.com>
2023-04-18 18:27:28 +0300

    Update script.js
    updata

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch('https://codex-mfmy.onrender.com', {
+  const response = await fetch('https://iditech.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
